a5d86d537c72830c9810e7b204bccbb95ca50490
KuntaKentai <kuntakentai@danworld.org>
2016-02-27 14:14:24 -0600

    Fixed some crashes with null player return

--- src/com/kunta/dev/PartyCommand.java
+++ src/com/kunta/dev/PartyCommand.java
@@ -16,10 +16,15 @@ public class PartyCommand implements CommandExecutor {
             /**(args[0],args[1],args[2]) is /party arg0 arg1 arg2
              * (args.length = one, two, or three is /party one two three**/
             /**Invite Start**/
+            if(args.length == 0){
+            	
+            	return true;
+            }
             if(args[0].equalsIgnoreCase("invite")){
             	if(args.length > 1){
             		target = PartyHandler.getPlayer(player, args[1]);
-            		PartyHandler.inviteToParty(player, target);
+            		if(target != null)
+            			PartyHandler.inviteToParty(player, target);
             		return true;
             	}
             	if(args.length ==1){
@@ -32,7 +37,8 @@ public class PartyCommand implements CommandExecutor {
             if(args[0].equalsIgnoreCase("join")){
             	if(args.length > 1){
             		target = PartyHandler.getPlayer(player, args[1]);
-            		PartyHandler.joinParty(target, player);
+            		if(target != null)
+            			PartyHandler.joinParty(target, player);
             		return true;
             	}
             	if(args.length ==1){
@@ -44,11 +50,16 @@ public class PartyCommand implements CommandExecutor {
             if(args[0].equalsIgnoreCase("list")){
             	if(args.length > 1){
             		target = PartyHandler.getPlayer(player, args[1]);
-            		PartyHandler.getParty(player, target);
+            		if(target != null)
+            			PartyHandler.getParty(player, target);
             		return true;
             	}
-            	if(args.length ==1){
-            		player.sendMessage("Please provide a players name to view their party");
+            	if(args.length == 1){
+            		if(PartyHandler.playerInParty(player)){
+            			PartyHandler.getParty(player, player);
+            			return true;
+            		} else
+            			player.sendMessage("Please provide a players name to view their party");
             		return true;
             	}
             }
@@ -57,7 +68,8 @@ public class PartyCommand implements CommandExecutor {
             if(args[0].equalsIgnoreCase("kick")){
             	if(args.length > 1){
             		target = PartyHandler.getPlayer(player, args[1]);
-            		PartyHandler.kickFromParty(target);
+            		if(target != null)
+            			PartyHandler.kickFromParty(target);
             		return true;
             	}
             	if(args.length ==1){
@@ -82,6 +94,18 @@ public class PartyCommand implements CommandExecutor {
             	}
             }
             /**Invites End**/
+            if(args[0].equalsIgnoreCase("create")){
+            	if(args.length == 1){
+            		PartyHandler.createParty(player, player.getName());;
+            		return true;
+            	}
+            }
+            if(args[0].equalsIgnoreCase("disband")){
+            	if(args.length == 1){
+            		PartyHandler.createParty(player, player.getName());;
+            		return true;
+            	}
+            }
             return true;
         }
 
--- src/com/kunta/dev/PartyHandler.java
+++ src/com/kunta/dev/PartyHandler.java
@@ -1,5 +1,7 @@
 package com.kunta.dev;
 
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -10,24 +12,42 @@ import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class PartyHandler{
 	// <string, string> is <partyName, playername>
 	static HashMap<String, List<String>> parties = new HashMap<String, List<String>>();
 	static HashMap<String, List<String>> invites = new HashMap<String, List<String>>();
+	static List<String> commands = new ArrayList<String>();
 	
-	public static void createParty(String PlayerName)
+	static int partyCap = 6;
+	
+	public static void createParty(Player host, String PlayerName)
 	{
 		if(!parties.containsKey(PlayerName)){
 			List<String> players = new ArrayList<String>();
 			players.add(PlayerName);
 			if(parties.containsValue(PlayerName)){
-				//leaveParty();
+				leaveParty(host);
 			}
 			parties.put(PlayerName, players);
 		}
 	}
-	public static void deleteParty(){}
+	public static void confirmDeleteParty(Player host)
+	{
+		if(playerInParty(host)){
+			host.sendMessage(ChatColor.BLUE + "Are you sure you want to disband the party?");
+		}
+	}
+	public static void deleteParty(Player host)
+	{
+		String hostName = host.getName();
+		if(playerInParty(host)){
+			partyBroadcast(host, ChatColor.BLUE + "The party has been disbanded!");
+			parties.remove(hostName);
+		}
+	}
 	public static void getInvites(Player asking)
 	{
 		String name = asking.getName();
@@ -63,21 +83,40 @@ public class PartyHandler{
 		return "Player not in party";
 	} 
 	public static Player getPlayer(Player sender, String target) {
-	    Player p = sender.getServer().getPlayer(target);
-	    return p;
+		List<String> guess = new ArrayList<String>();
+		try {
+			for(Player player : sender.getServer().getOnlinePlayers()){
+				guess.add(player.getName());
+			}
+			if(guess.contains(target)){
+				Player p = sender.getServer().getPlayer(target);
+				return p;
+			} else
+				sender.sendMessage(ChatColor.RED + "Player does not exist.");
+		} catch(IllegalArgumentException ignored) {}
+		return null;
 	}
 	public static void inviteToParty(Player host, Player invited)
 	{
+		//if(host.getServer().getOnlinePlayers().contains(invited.getName())){
 		String hostName = host.getName();
 		String invitedName = invited.getName();
 		List<String> invs = new ArrayList<String>();
+		List<String> party = parties.get(hostName);
 		if(!parties.containsKey(hostName)){
-    		createParty(hostName);
+    		createParty(host, hostName);
 		}
-		invited.sendMessage(ChatColor.GREEN + hostName + ChatColor.BLUE + " invited you to join their party!");
-		invs.add(hostName);
-		invites.put(invitedName, invs);
-		host.sendMessage(ChatColor.BLUE + "Successfully invited " + ChatColor.GREEN + invitedName + ChatColor.BLUE + " to your party!");
+		if(hostName != invitedName && party.size() < partyCap){
+			invited.sendMessage(ChatColor.GREEN + hostName + ChatColor.BLUE + " invited you to join their party!");
+			invs.add(hostName);
+			invites.put(invitedName, invs);
+			host.sendMessage(ChatColor.BLUE + "Successfully invited " + ChatColor.GREEN + invitedName + ChatColor.BLUE + " to your party!");
+		} else
+			if(party.size() == partyCap){
+				host.sendMessage(ChatColor.RED + "Cannot invite player! \nParty is full!");
+			} else
+			host.sendMessage(ChatColor.RED + "Cannot join your own party!");
+		//}
 	}
 	public static void joinParty(Player host, Player joining)
 	{
@@ -88,15 +127,18 @@ public class PartyHandler{
 			if(invites.get(joinName) != null){
 				List<String> invs = invites.get(joinName);
 				if(invs.contains(hostName)){
-					party.add(joinName);
-					parties.put(hostName, party);
-					for(int i = 0; i < invs.size(); i++){
-						if(invs.get(i) == hostName){
-							invs.remove(i);
+					if(party.size() < partyCap){
+						partyBroadcast(host, ChatColor.GREEN + joining.getName() + ChatColor.BLUE + " joined your party!");
+						party.add(joinName);
+						parties.put(hostName, party);
+						for(int i = 0; i < invs.size(); i++){
+							if(invs.get(i) == hostName){
+								invs.remove(i);
+							}
 						}
-					}
-		    		host.sendMessage(ChatColor.GREEN + joining.getName() + ChatColor.BLUE + " joined your party!");
-		    		joining.sendMessage(ChatColor.BLUE + "Successfully joined " + ChatColor.GREEN + host.getName() + "'s " + ChatColor.BLUE + "party!");
+			    		joining.sendMessage(ChatColor.BLUE + "Successfully joined " + ChatColor.GREEN + host.getName() + "'s " + ChatColor.BLUE + "party!");
+					} else
+						joining.sendMessage(ChatColor.RED + "Party is full!");
 				} 
 			} else
 				joining.sendMessage(ChatColor.RED + "You have not been invited to this party!");
@@ -118,6 +160,7 @@ public class PartyHandler{
 					parties.put(hostName, party);
 					host.sendMessage(ChatColor.BLUE + "Kicked " + ChatColor.GREEN + kickedName + ChatColor.BLUE + " from your party!");
 					kicked.sendMessage(ChatColor.RED + "You were kicked from the party!");
+					partyBroadcast(host, ChatColor.GREEN + kickedName + ChatColor.BLUE + " was kicked from your party!");
 				}
 			}
 		}
@@ -129,11 +172,52 @@ public class PartyHandler{
 		List<String> party = parties.get(hostName);
 		for(int i = 0; i < party.size(); i++){
 			if(party.get(i) == leaveName){
+				partyBroadcast(leave, ChatColor.GREEN + leaveName + ChatColor.BLUE + " has left the party!");
 				party.remove(i);
 				leave.sendMessage(ChatColor.BLUE + "You have left the party");
 			}
 		}
 		
 	}
-
+	public static void loadCommands()
+	{
+		commands.add("invite");
+		commands.add("join");
+		commands.add("list");
+		commands.add("kick");
+		commands.add("leave");
+		commands.add("invites");
+		commands.add("create");
+		commands.add("disband");
+	}
+	public static void partyBroadcast(Player p, String message)
+	{
+		String partyHost = getPartyHost(p);
+		List<String> party = parties.get(partyHost);
+		for(String name : party){
+			Player player = getPlayer(p, name);
+			player.sendMessage(message);
+		}
+	}
+	public static void partyHelp(Player player)
+	{
+		player.sendMessage("");
+	}
+	public static Boolean playerInParty(Player player)
+	{
+		String hostName = player.getName();
+		if(parties.containsKey(hostName)){
+			return true;
+		}
+		return false;
+	}
+	
+	/**Test Area**/
+	public static UUID fetchUUID(String s) throws Exception{
+        URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + s);
+        String uuid = (String)((JSONObject)new JSONParser().parse(new InputStreamReader(url.openStream()))).get("id");
+        String realUUID = uuid.substring(0, 8) + "-" + uuid.substring(8, 12) + "-" + uuid.substring(12, 16) + "-" + uuid.substring(16, 20) + "-" + uuid.substring(20, 32);
+        return UUID.fromString(realUUID);
+	}
+	/**Test Area**/
 }
